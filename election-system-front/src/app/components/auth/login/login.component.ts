1c3d1bde6b80ad739328b71c10d355928f86002c
Damian Etcheverrigaray <damian.etcheverrigar@estudiantes.utec.edu.uy>
2025-01-23 15:30:18 -0300

    refactor login component and update styles; move assets to correct directory

--- election-system-front/src/app/components/auth/login/login.component.ts
+++ election-system-front/src/app/components/auth/login/login.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { MessagesModule } from 'primeng/messages';
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    ButtonModule, 
+    InputTextModule, 
+    CheckboxModule, 
+    FormsModule, 
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  
+  rememberMe: boolean = false;
+  filterLoaded = signal(false);
 
+  constructor(
+      private router: Router,
+      private activatedRoute: ActivatedRoute
+    ) {}
+
+
+  login(){
+
+  }
+
+  showErrorViaMessages(msg : string) {
+
+  }
 }
